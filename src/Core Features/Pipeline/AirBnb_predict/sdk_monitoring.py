c3ada1adff651e82813df3d8cb5c9a81349ecab0
Florian Natterer <natterer.flo99@gmail.com>
2022-05-15 16:24:36 +0200

    Example for Monitoring through the SDK

---
+++ src/Core Features/Pipeline/AirBnb_predict/sdk_monitoring.py
@@ -0,0 +1,29 @@
+from clearml import Task
+import matplotlib.pyplot as plt
+
+
+task_list: list[Task] = Task.get_tasks(
+    project_name="demo/Fashion MNIST/.pipelines/Fashion MNIST Pipeline",
+    task_name="model_training",
+    task_filter={
+        "status": ["completed"]
+    }
+)
+
+task_performance: dict[str: dict] = {}
+
+
+for task in task_list:
+    try:
+        epoch_accuracy = task.get_last_scalar_metrics()["epoch_accuracy"]["epoch_accuracy"]
+        task_performance[task.id] = epoch_accuracy["max"]
+    except KeyError:
+        continue
+
+
+for key in sorted(task_performance, key=task_performance.get, reverse=True):
+    print(f"{key}: {task_performance[key]}")
+
+
+
+
